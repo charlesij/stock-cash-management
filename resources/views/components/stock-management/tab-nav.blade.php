1549f89ab561c4d4932ce0f240e9d0a4a3ca03f2
Tev Immanuel <168507263+daunkering21@users.noreply.github.com>
2025-08-06 11:46:46 +0700

    Update component
    addition of stock management componentss

--- resources/views/components/stock-management/tab-nav.blade.php
+++ resources/views/components/stock-management/tab-nav.blade.php
@@ -1,3 +1,13 @@
-<div>
-    <!-- Well begun is half done. - Aristotle -->
+<div class="border-b border-gray-200">
+    <nav class="-mb-px flex space-x-8" aria-label="Tabs">
+        <a href="{{ route('stocks.index') }}" class="{{ request()->routeIs('stocks.index') ? 'border-blue-500 text-blue-600' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300' }} whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm">
+            Inventory
+        </a>
+        <a href="{{ route('stocks.settings') }}" class="{{ request()->routeIs('stocks.settings') ? 'border-blue-500 text-blue-600' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300' }} whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm">
+            Settings
+        </a>
+        <a href="{{ route('stocks.history') }}" class="{{ request()->routeIs('stocks.history') ? 'border-blue-500 text-blue-600' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300' }} whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm">
+            History
+        </a>
+    </nav>
 </div>
---
+++ resources/views/components/table-action/overlay.blade.php
@@ -0,0 +1,14 @@
+<div class="overlay-menu fixed w-48 bg-white rounded-md shadow-lg py-1 ring-1 ring-black ring-opacity-5 z-50 table-action-menu hidden">
+    <a class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer">
+        <i class="fa-solid fa-filter mr-2"></i>
+        Filter
+    </a>
+    <a class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer">
+        <i class="fa-solid fa-sort mr-2"></i>
+        Sort
+    </a>
+    <a class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer">
+        <i class="fa-solid fa-download mr-2"></i>
+        Export
+    </a>
+</div>
