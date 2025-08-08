975fa3a1a75c5c4f439fc0ab14d9de71f4df50b9
Tev Immanuel <168507263+daunkering21@users.noreply.github.com>
2025-08-08 21:05:17 +0700

    Stock Controller store method success

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -2,16 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Produk;
 use App\Models\Satuan;
 use App\Models\Supplier;
+use App\Models\ProdukDetail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class StockController extends Controller
 {
     public function stockView()
     {
         $breadcrumb = [
-            ['name' => 'Stock', 'url' => route('stocks.index')],
+            ['name' => 'Stock', 'url' => route('stock.index')],
         ];
         return view('dashboard.stock.index', [
             'breadcrumb' => $breadcrumb
@@ -21,7 +24,7 @@ class StockController extends Controller
     public function create()
     {
         $breadcrumb = [
-            ['name' => 'Stock', 'url' => route('stocks.index')],
+            ['name' => 'Stock', 'url' => route('stock.index')],
             ['name' => 'Create', 'url' => route('stock.create')],
         ];
         $satuan = Satuan::get();
@@ -35,29 +38,62 @@ class StockController extends Controller
 
     public function store(Request $request)
     {
-        dd($request->all());
-    }
+        try {
+            $validatedData = $request->validate([
+                'item_name' => 'required|string',
+                'item_harga_beli' => 'required|numeric',
+                'item_tanggal_masuk' => 'required|date',
+                'item_supplier' => 'required|string',
+                'item_metode_pembayaran' => 'required|string',
+                'item_kuantitas' => 'required|array',
+                'item_kuantitas.*' => 'required|numeric',
+                'item_unit_satuan' => 'required|array',
+                'item_unit_satuan.*' => 'required|string',
+                'item_harga_jual' => 'required|array',
+                'item_harga_jual.*' => 'required|numeric',
+                'item_keterangan' => 'nullable|string',
+                'item_jatuh_tempo' => 'nullable',
+            ]);
+            DB::beginTransaction();
 
-    public function createTest()
-    {
-        $breadcrumb = [
-            ['name' => 'Stock', 'url' => route('stocks.index')],
-            ['name' => 'Create', 'url' => route('stock.create.test')],
-        ];
-        $satuan = Satuan::get();
-        // $supplier = Supplier::get();
-        return view('dashboard.stock.create-test', [
-            'breadcrumb' => $breadcrumb,
-            'satuan' => $satuan,
-            // 'supplier' => $supplier
-        ]);
+            $produk = Produk::create([
+                'nama' => $validatedData['item_name'],
+                'harga_beli' => $validatedData['item_harga_beli'],
+                'metode_pembayaran' => $validatedData['item_metode_pembayaran'],
+                'data_supplier' => $validatedData['item_supplier'],
+                'tanggal_barang_masuk' => $validatedData['item_tanggal_masuk'],
+                'tanggal_jatuh_tempo' => $validatedData['item_jatuh_tempo'],
+                'keterangan' => $validatedData['item_keterangan'],
+            ]);
+
+            for ($i = 0; $i < count($validatedData['item_unit_satuan']); $i++) {
+                $log[] = [
+                    'produk_id' => $produk->id,
+                    'nama_satuan' => $validatedData['item_unit_satuan'][$i],
+                    'kuantitas' => $validatedData['item_kuantitas'][$i],
+                    'harga_jual' => $validatedData['item_harga_jual'][$i],
+                ];
+                ProdukDetail::create([
+                    'produk_id' => $produk->id,
+                    'nama_satuan' => $validatedData['item_unit_satuan'][$i],
+                    'kuantitas' => $validatedData['item_kuantitas'][$i],
+                    'harga_jual' => $validatedData['item_harga_jual'][$i],
+                ]);
+            }
+
+            DB::commit();
+            return redirect()->route('stock.index')->with('success', 'Stock created successfully');
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return redirect()->route('stock.create')->with('error', 'Stock created failed: ' . $e->getMessage());
+        }
     }
 
     public function createSatuan(Request $request)
     {
         try {
             $request->validate([
-                'new_satuan_name' => 'required|string|max:255',
+                'new_satuan_name' => 'required|string|max:255|unique:satuan,nama',
             ]);
 
             Satuan::create([
