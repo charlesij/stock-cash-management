059ffa7226a576a89ed89b619cdb2ff95e1ccd23
Tev Immanuel <168507263+daunkering21@users.noreply.github.com>
2025-08-08 21:06:52 +0700

    Transaction Controller update
    Store and logging complete

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SaldoKas;
+use App\Models\TransaksiKas;
 use Illuminate\Http\Request;
+use App\Models\HistoryTransaksi;
+use Illuminate\Support\Facades\Log;
 
 class TransactionController extends Controller
 {
@@ -20,15 +24,83 @@ class TransactionController extends Controller
         $breadcrumb = [
             ['name' => 'Transaction', 'url' => route('transaction.cashflow')],
         ];
+
+        $saldoKas = TransaksiKas::orderByDesc('updated_at')->paginate(20);
+
         return view('dashboard.transaction.cashflow', [
+            'breadcrumb' => $breadcrumb,
+            'saldoKas' => $saldoKas,
+        ]);
+    }
+    public function cashflowCreate()
+    {
+        $breadcrumb = [
+            ['name' => 'Transaction', 'url' => route('transaction.cashflow')],
+            ['name' => 'Kelola Kas', 'url' => route('transaction.cashflow.create')],
+        ];
+
+        return view('dashboard.transaction.cashflow.create', [
             'breadcrumb' => $breadcrumb
         ]);
+    }
+    public function cashflowStore(Request $request)
+    {
+        try {
+            $request->validate([
+                'modal' => 'required|string',
+                'keterangan' => 'required|string',
+            ]);
+
+            $kasSaldo = SaldoKas::where('date', date('Y-m-01'))->first();
+            
+            if (!$kasSaldo) {
+                $kasSaldo = SaldoKas::create([
+                    'date' => date('Y-m-01'),
+                    'cash' => (float) str_replace('.', '', $request->modal),
+                    'hutang' => 0,
+                    'keterangan' => $request->keterangan,
+                ]);
+            } else {
+                $kasSaldo->update([
+                    'cash' => $kasSaldo->cash + (float) str_replace('.', '', $request->modal),
+                    'keterangan' => $request->keterangan,
+                ]);
+            }
+
+            $newTransaksiKas = [
+                'saldo_kas_id' => $kasSaldo->id,
+                'jenis_transaksi' => 'modal masuk',
+                'keterangan' => $request->keterangan,
+                'cash_in' => (float) str_replace('.', '', $request->modal),
+                'cash_out' => 0,
+                'current_saldo' => $kasSaldo->cash,
+            ];
+            
+            $historyFormat = [
+                'jenis_transaksi' => 'modal masuk',
+                'keterangan' => $request->keterangan,
+                'cash_in' => (float) str_replace('.', '', $request->modal),
+                'cash_out' => 0,
+                'hutang_in' => 0,
+                'hutang_out' => 0,
+            ];
+            
+            TransaksiKas::create($newTransaksiKas);
+            HistoryTransaksi::storeTransactionHistory($historyFormat);
+            return redirect()->route('transaction.cashflow')->with('success', 'Data berhasil ditambahkan');
+        } catch (\Exception $e) {
+            Log::error('Error storing transaction history: ' . $e->getMessage());
+            return redirect()->route('transaction.cashflow.create')->with('error', 'Gagal menambahkan data: ' . $e->getMessage());
+        }
     }
     public function debtView()
     {
         $breadcrumb = [
             ['name' => 'Transaction', 'url' => route('transaction.debt')],
         ];
+
+        $monthYear = date('Y-m-01');
+
         return view('dashboard.transaction.debt', [
             'breadcrumb' => $breadcrumb
         ]);
@@ -56,8 +128,12 @@ class TransactionController extends Controller
         $breadcrumb = [
             ['name' => 'Transaction', 'url' => route('transaction.history')],
         ];
+
+        $history = HistoryTransaksi::orderByDesc('created_at')->paginate(20);
+
         return view('dashboard.transaction.history', [
-            'breadcrumb' => $breadcrumb
+            'breadcrumb' => $breadcrumb,
+            'transactionHistory' => $history,
         ]);
     }
 }
